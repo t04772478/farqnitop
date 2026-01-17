00fb9c66132d188bc630f7eaaa8df17aef3cc7f4
t04772478 <t04772478@gmail.com>
2026-01-17 15:30:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,37 @@ tg.expand();
 
 let score = 0;
 
+// FARQLAR (px bo‘yicha)
+const differences = [
+  { name: "quloq", x: 60, y: 40, found: false },
+  { name: "ko'z",  x: 95, y: 75, found: false },
+  { name: "pishloq", x: 140, y: 120, found: false }
+];
+
 document.querySelectorAll(".img-wrap").forEach(box => {
   box.addEventListener("click", (e) => {
 
     const rect = box.getBoundingClientRect();
+    const clickX = e.clientX - rect.left;
+    const clickY = e.clientY - rect.top;
 
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
+    differences.forEach(d => {
+      if (!d.found &&
+          Math.abs(d.x - clickX) < 15 &&
+          Math.abs(d.y - clickY) < 15) {
 
-    const mark = document.createElement("div");
-    mark.className = "mark";
-    mark.style.left = (x - 15) + "px";
-    mark.style.top = (y - 15) + "px";
+        d.found = true;
+        score++;
 
-    box.appendChild(mark);
+        const mark = document.createElement("div");
+        mark.className = "mark";
+        mark.style.left = (d.x - 15) + "px";
+        mark.style.top  = (d.y - 15) + "px";
 
-    score++;
-    document.getElementById("score").innerText = "Ball: " + score;
+        box.appendChild(mark);
+        document.getElementById("score").innerText = "Ball: " + score;
+      }
+    });
+
   });
 });
