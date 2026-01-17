1c906d48b129f3fc1e38a316d39873a160b8f252
t04772478 <t04772478@gmail.com>
2026-01-17 15:27:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-alert("JS yuklandi");
+const tg = window.Telegram.WebApp;
+tg.ready();
+tg.expand();
 
-document.body.addEventListener("click", () => {
-  alert("Bosildi!");
+let score = 0;
+
+document.querySelectorAll(".img-wrap").forEach(box => {
+  box.addEventListener("click", (e) => {
+
+    const rect = box.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const mark = document.createElement("div");
+    mark.className = "mark";
+    mark.style.left = (x - 15) + "px";
+    mark.style.top = (y - 15) + "px";
+
+    box.appendChild(mark);
+
+    score++;
+    document.getElementById("score").innerText = "Ball: " + score;
+  });
 });
