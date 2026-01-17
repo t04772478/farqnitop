0ea7f276068b59ab445eb712bfbaec328d14ff13
t04772478 <t04772478@gmail.com>
2026-01-17 15:24:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-const tg = window.Telegram.WebApp;
-tg.ready();
-tg.expand();
+alert("JS yuklandi");
 
-let score = 0;
-
-document.querySelectorAll('.img-wrap').forEach(box => {
-  box.addEventListener('click', (e) => {
-    const rect = box.getBoundingClientRect();
-
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    const mark = document.createElement('div');
-    mark.className = 'mark';
-    mark.style.left = (x - 15) + 'px';
-    mark.style.top = (y - 15) + 'px';
-
-    box.appendChild(mark);
-
-    score++;
-    document.getElementById('score').innerText = "Ball: " + score;
-  });
+document.body.addEventListener("click", () => {
+  alert("Bosildi!");
 });
