7b48452cb389417277e88ce19ec1bec70e8cd89a
t04772478 <t04772478@gmail.com>
2026-01-17 15:21:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,32 +4,21 @@ tg.expand();
 
 let score = 0;
 
-// FARQLAR RO‘YXATI (foizda)
-const differences = [
-  { x: 50, y: 40, found: false }
-];
-
 document.querySelectorAll('.img-wrap').forEach(box => {
   box.addEventListener('click', (e) => {
     const rect = box.getBoundingClientRect();
-    const x = ((e.clientX - rect.left) / rect.width) * 100;
-    const y = ((e.clientY - rect.top) / rect.height) * 100;
 
-    differences.forEach(d => {
-      if (!d.found &&
-          Math.abs(d.x - x) < 5 &&
-          Math.abs(d.y - y) < 5) {
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
 
-        d.found = true;
-        score++;
-        document.getElementById('score').innerText = "Ball: " + score;
+    const mark = document.createElement('div');
+    mark.className = 'mark';
+    mark.style.left = (x - 15) + 'px';
+    mark.style.top = (y - 15) + 'px';
 
-        const mark = document.createElement('div');
-        mark.className = 'mark';
-        mark.style.left = (d.x - 2) + '%';
-        mark.style.top = (d.y - 2) + '%';
-        box.appendChild(mark);
-      }
-    });
+    box.appendChild(mark);
+
+    score++;
+    document.getElementById('score').innerText = "Ball: " + score;
   });
 });
